fe1d1ce40a32be289e7590073f97b6bd2fd6619b
Anatoly <anatolyuss@gmail.com>
2020-07-16 19:35:09 +0300

    added props and state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,60 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person';
+import Person from './Person/Person.js';
 
 class App extends Component {
-  getAge() {
+  constructor(props) {
+    super(props);
+  }
+
+  getAge = () => {
     return Math.floor(Math.random() * 30) || 120;
+  }
+
+  state = {
+    sample_attribute: 'sample sample',
+    persons: [
+      { name: 'Anna', age: this.getAge(), inner_text: undefined },
+      { name: 'Bella', age: this.getAge(), inner_text: 'I\'m good!' },
+    ]
+  }
+
+  switchPersonsHandler = () => {
+    const tempPerson = this.state.persons[0];
+    this.state.persons[0] = this.state.persons[1];
+    this.state.persons[1] = tempPerson;
+
+    this.setState(this.state.persons);
+  }
+
+  nameChangedHandler = event => {
+    this.state.persons[0].name = event.target.value;
+
+    this.setState(this.state.persons);
   }
 
   render() {
     return (
       <div className="App">
         <h1>Hey, I'm a React app!</h1>
-        <Person age={this.getAge()} />
-        <Person age={this.getAge()}>I'm good!</Person>
+        <h2>{this.state.sample_attribute}</h2>
+        <button onClick={this.switchPersonsHandler}>Switch persons</button>
+
+        <Person
+          click={this.switchPersonsHandler} changed={this.nameChangedHandler}
+          name={this.state.persons[0].name} age={this.state.persons[0].age}
+        >{this.state.persons[0].inner_text}
+        </Person>
+        <Person
+          click={this.switchPersonsHandler} changed={this.nameChangedHandler}
+          name={this.state.persons[1].name} age={this.state.persons[1].age}
+        >{this.state.persons[1].inner_text}
+        </Person>
+        {/*{*/}
+        {/*  this.state.persons.forEach(person => {*/}
+        {/*    <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>{this.state.persons[1].inner_text}</Person>*/}
+        {/*  })*/}
+        {/*}*/}
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 // import Person from './Person.css';
 
 class Person extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <div>
-        <p>I am a {this.props.age} years old Person!</p>
-        <p>{this.props.children}</p>
+        <p>I am a {this.props.age} years old {this.props.name}!</p>
+        <p onClick={this.props.click}>{this.props.children || 'No Text'}</p>
+        <input type="text" onChange={this.props.changed} value={this.props.name} />
       </div>
     );
   }
